87a7e7c27aeaad51b946c05cdd0a9b12a899437d
Cameron Elliott <CameronElliott@Camerons-MacBook-Pro-4.local>
2018-02-24 10:36:56 -0800

    yo

---
+++ Cameron-Test.js
@@ -0,0 +1,15 @@
+module.exports = {
+    assert: function(expected, actual, message = "Test") {
+        if (expected === actual) {
+            console.log('👍🏼  ' + message)
+        }
+         else {
+        diff = util.inspect(expected) + "\n   " + util.inspect(actual);
+        console.log('💀  ' + message + "\n   Assertion failed no match\n   " + diff);
+        }
+    },
+assert_true: function(actual, message) {   
+},
+assert_false: function(actual, message) {   
+}
+};
